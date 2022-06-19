8337d4d1ac508399a890110b9aab81521552fc8b
paulweezydesign <weezy@paulweezydesign.com>
2022-06-18 17:55:21 -0700

    2 files modified

--- my-app.js
+++ my-app.js
@@ -71,11 +71,23 @@ class App extends LitElement {
 
   render() {
     return html`
+    <nav class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
+        <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+            <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                <a class="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                    href="/">My Cute Kittens</a>
+                <button
+                    class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                    type="button" onclick="toggleNavbar('example-collapse-navbar')">
+                    <i class="text-white fas fa-bars"></i>
+                </button>
+            </div>
+
       <app-link href="/">Home</app-link>
       <app-link href="/about">About</app-link>
       <app-link href="kittens">Kittens</app-link>
       <app-link href="contact">Contact</app-link>
-
+    </nav>
       <app-main active-route=${this.route}>
         <div route="home">
           <home-page></home-page>
